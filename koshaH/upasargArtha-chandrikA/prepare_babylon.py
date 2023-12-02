7045c4012c3322d30d4d6eaacb3f9f4ffb5552e5
drdhaval2785 <drdhaval2785@gmail.com>
2023-12-02 22:20:06 +0530

    added a pari component which was erroneously copied in some other file

--- koshaH/upasargArtha-chandrikA/prepare_babylon.py
+++ koshaH/upasargArtha-chandrikA/prepare_babylon.py
@@ -1,44 +1,57 @@
 import re
 import glob
 
-def obsolete_change_hash_to_dash():
-	content_files = glob.glob('trial/*.md')
+header = """
+#bookname=upasargArthachandrikA (sa-sa)
+#stripmethod=keep
+#sametypesequence=h
+#description=https://github.com/indic-dict/stardict-sanskrit-vyAkaraNa/issues/10
+
+"""
+
+def prepare_babylon():
+	content_files = glob.glob('*.md')
 	content_files = [f for f in content_files if 'index.md' not in f]
+	fileout = 'upasargArthachandrikA.babylon'
+	fout = open(fileout, 'w')
+	fout.write(header)
 	for filein in content_files:
 		print(filein)
 		fin = open(filein, 'r')
 		data = fin.read()
 		fin.close()
-		# make the necessary change
-		data = re.sub('\n###[ ]*(.*)[ ]*\n', '\n- {\g<1>}\n', data)
-		fileout = filein
-		fout = open(fileout, 'w')
-		fout.write(data)
-		fout.close()
+		parts = data.split('##')
+		# Discarding the top title part
+		parts_of_interest = parts[1:]
+		for p in parts_of_interest:
+			itms = p.split('-')
+			dhAtu = itms[0]
+			dhAtu = dhAtu.rstrip().lstrip()
+			sopasarga = itms[1]
+			sopasarga = sopasarga.rstrip('}\n').lstrip('- {')
+			definition = '-'.join(itms[2:])
+			definition = '-' + definition
+			definition = definition.rstrip()
+			definition = definition.replace('\n', '<br>')
+			hws = dhAtu + '|' + sopasarga
+			print(hws)
+			#print(definition)
+			#print()
+			fout.write(hws + '\n' + definition + '\n\n')
+
+	fout.close()
+
+	babylon_file = 'upasargArthachandrikA.babylon'
+	fin = open(babylon_file, 'r')
+	data = fin.read()
+	data = data.rstrip()
+	fin.close()
+	fout = open(babylon_file, 'w')
+	fout.write(data)
+	fout.close()
+
+
 
 if __name__ == "__main__":
-	filein = '212_upa.md'		
-	print(filein)
-	fin = open(filein, 'r')
-	data = fin.read()
-	fin.close()
-	parts = data.split('##')
-	# Discarding the top title part
-	parts_of_interest = parts[1:]
-	for p in parts_of_interest:
-		itms = p.split('-')
-		dhAtu = itms[0]
-		dhAtu = dhAtu.rstrip().lstrip()
-		sopasarga = itms[1]
-		sopasarga = sopasarga.rstrip('}\n').lstrip('- {')
-		definition = '-'.join(itms[2:])
-		definition = '-' + definition
-		definition = definition.rstrip()
-		definition = definition.replace('\n', '<br>')
-		hws = dhAtu + '|' + sopasarga
-		print(hws)
-		print(definition)
-		print()
-
-	# make the necessary change
+	prepare_babylon()
 
